dd589bc5c3a206215d6acfb269c4d376fb1b6177
bufgix <ooruc471@yandex.com>
2020-07-07 03:43:23 +0300

    refactor: use rc-slider

--- components/Player.js
+++ components/Player.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Slider from "rc-slider";
 
 import styles from "./Player.module.scss";
 import PlayButton from "../icons/PlayIcon";
@@ -22,15 +23,23 @@ function Player() {
         <li>Date: 01.09.2020</li>
       </ul>
 
-      <input
+      {/*       <input
         className={styles.seekbar}
         type="range"
         name="seekbar"
         id="seekbar"
         min="0"
         max="100"
+      /> */}
+      <Slider
+        railStyle={{ backgroundColor: "white", height: 6 }}
+        trackStyle={{ backgroundColor: "#bd00ff", height: 6 }}
+        handleStyle={{
+          borderColor: "#bd00ff",
+          backgroundColor: "#bd00ff",
+        }}
+        className={styles.seekbar}
       />
-
       {isPlay ? (
         <PlayButton
           className={styles.actionbtn}
@@ -46,7 +55,7 @@ function Player() {
           onClick={() => togglePlay()}
         />
       )}
-      <ArrowIcon className={styles.arrow} height="20" width="50"/>
+      <ArrowIcon className={styles.arrow} height="20" width="50" />
     </div>
   );
 }
