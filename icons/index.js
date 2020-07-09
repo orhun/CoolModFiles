57a02759983ce3b3b54ec176ce420651a5fffb5b
bufgix <ooruc471@yandex.com>
2020-07-10 01:55:44 +0300

    refactor: collect all icons into the directory

---
+++ icons/index.js
@@ -0,0 +1,9 @@
+import PlayButton from "./PlayIcon";
+import PauseButton from "./PauseIcon";
+import ArrowIcon from "./ArrowIcon";
+import DownloadButton from "./DownloadIcon";
+import RightButton from "./RightIcon";
+import LeftButton from "./LeftIcon";
+
+
+export {PlayButton, PauseButton, ArrowIcon, DownloadButton, RightButton, LeftButton}
