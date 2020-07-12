31b8ec6c50adf1fa9546889fe77971cefc2b57ee
orhun <orhunparmaksiz@gmail.com>
2020-07-12 23:50:12 +0200

    chore: Add and initialize Google Analytics module

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactGA from "react-ga";
 import Player from "../components/Player";
 import Footer from "../components/Footer";
 import {
@@ -23,6 +24,8 @@ function Index({ trackId }) {
   }, [enterKey]);
 
   React.useEffect(() => {
+    ReactGA.initialize("UA-172416216-1");
+    ReactGA.pageview(window.location.pathname + window.location.search);
     document.getElementById(
       "app"
     ).style.backgroundImage = `url('/images/${getRandomFromArray(BG_IMAGES)}')`;
